5d23834d43ad54f273851e229d3f6053131f2ce3
Billal Asif <asif.billal@outlook.com>
2022-04-16 20:46:48 +0100

    Parallex added on main image

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, {
+    let elems = document.querySelectorAll('.fixed-action-btn');
+    let instances = M.FloatingActionButton.init(elems, {
       direction: 'top',
       hoverEnabled: false
     });
   });
 
   document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
+    let elems = document.querySelectorAll('.sidenav');
+    let instances = M.Sidenav.init(elems, 'left');   
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    let elems = document.querySelectorAll('.parallax');
+    let instances = M.Parallax.init(elems, 991);
   });
