56089a0c95893f01c4f70af93070add0886f2652
Billal Asif <asif.billal@outlook.com>
2022-04-16 13:46:51 +0100

    nav changed to materlize nav

--- js/script.js
+++ js/script.js
@@ -5,3 +5,8 @@ document.addEventListener('DOMContentLoaded', function() {
       hoverEnabled: false
     });
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
