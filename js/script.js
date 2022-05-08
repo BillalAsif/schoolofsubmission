3ca63185c4c723b1a37852d588f450f076143369
Billal Asif <asif.billal@outlook.com>
2022-05-08 16:18:34 +0100

    carousel added in about section

--- js/script.js
+++ js/script.js
@@ -71,3 +71,11 @@ function scrollFunction() {
 scrollButton.addEventListener("click", () => {
   homeSection.scrollIntoView({ behavior: 'smooth' });
 })
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, {
+    indicators: false,
+    padding: 200,
+  });
+});
