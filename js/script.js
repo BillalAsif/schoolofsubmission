f31248cbffcf3be6ad1edb9f26845bee8bbc82b9
Billal Asif <asif.billal@outlook.com>
2022-05-09 21:03:13 +0100

    owl carousel added and other minor changes with text, padding etc

--- js/script.js
+++ js/script.js
@@ -97,3 +97,20 @@ scrollButton.addEventListener("click", () => {
   homeSection.scrollIntoView({ behavior: 'smooth' });
 })
 
+//carousel
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    margin: 5,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 3,
+      }
+    }
+  });
+});
